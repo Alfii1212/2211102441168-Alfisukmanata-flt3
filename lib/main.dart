820a4a2c14e9e78c86b3d24fc6bb076fe1f8355e
Alfii1212 <145786131+Alfii1212@users.noreply.github.com>
2024-03-20 17:24:15 +0800

    container

--- lib/main.dart
+++ lib/main.dart
@@ -40,3 +40,17 @@ class MyHomePage extends StatelessWidget {
                 ),
                 textAlign: TextAlign.center,
               ),
+              SizedBox(height: 20.0),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.symmetric(horizontal: 40.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Text(
+                  'Container: Wadah serbaguna untuk widget lain, menyediakan dekorasi, padding, margin, dan pengaturan ukuran.',
+                  style: TextStyle(fontSize: 18.0),
+                  textAlign: TextAlign.center,
+                ),
+              ),
