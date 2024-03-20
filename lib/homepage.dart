ed286c0c758b42c4c535a19aba106fa1033acc40
Alfii1212 <145786131+Alfii1212@users.noreply.github.com>
2024-03-20 16:53:32 +0800

    first commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key, required String title});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,139 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/homepage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const Homepage(title: '',),
+      home: MyHomePage(),
     );
   }
 }
 
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Widget'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Widget Alfi Sukmanata',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.blue,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20.0),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.symmetric(horizontal: 40.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Text(
+                  'Container: Wadah serbaguna untuk widget lain, menyediakan dekorasi, padding, margin, dan pengaturan ukuran.',
+                  style: TextStyle(fontSize: 18.0),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(Icons.favorite, size: 50.0, color: Colors.red),
+                  Icon(Icons.star, size: 50.0, color: Colors.yellow),
+                  Icon(Icons.thumb_up, size: 50.0, color: Colors.blue),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Elevated Button'),
+              ),
+              SizedBox(height: 10.0),
+              TextButton(
+                onPressed: () {},
+                child: Text('Text Button'),
+              ),
+              SizedBox(height: 20.0),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    width: 200.0,
+                    height: 200.0,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  Icon(Icons.favorite, size: 100.0, color: Colors.red),
+                ],
+              ),
+              SizedBox(height: 20.0),
+              AspectRatio(
+                aspectRatio: 16 / 9,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: Image.network(
+                    'https://cdn.wallpapersafari.com/41/38/Mc7ELF.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Text(
+                    'Ini adalah contoh teks yang diletakkan di dalam Padding dan di-Center-kan.',
+                    style: TextStyle(fontSize: 18.0),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: EdgeInsets.symmetric(vertical: 10.0),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        child: Text('${index + 1}'),
+                      ),
+                      title: Text('Item ${index + 1}'),
+                      subtitle: Text('Subtitle ${index + 1}'),
+                      trailing: Icon(Icons.arrow_forward),
+                      onTap: () {},
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
